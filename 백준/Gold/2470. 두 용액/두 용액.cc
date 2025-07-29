a35c2a15e1abd5cbb861a21d00e03552c4502669
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-07-29 20:45:09 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int n; cin >> n;
+
+	vector<int> vec;
+	vector<int> result;
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		vec.push_back(num);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	int left, right;
+	left = 0; right = n - 1;
+	int min = 2e9;
+	while (left < right) {
+		int tmp = vec[left] + vec[right];
+
+		if (abs(tmp) <= min) {
+			min = abs(tmp);
+			result.clear();
+			result.push_back(vec[left]);
+			result.push_back(vec[right]);
+		}
+		if (tmp < 0) {
+			left++;
+		}
+		else {
+			right--;
+		}
+	}
+
+	for (auto r : result) {
+		cout << r << " ";
+	}
+
+}
