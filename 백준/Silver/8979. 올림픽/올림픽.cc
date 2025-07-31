b378a0425a3510fe36603606cdaf778c019a200a
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-08-01 01:52:10 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2028 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -8,9 +8,9 @@ int n, k;
 vector<vector<int>> vec;
 
 bool compare(vector<int> a, vector<int> b) {
-	if (a[1] == b[1] && a[2] == b[2]) return a[3] > b[3];
-	else if (a[1] == b[1]) return a[2] > b[2];
-	return a[1] > b[1];
+	if (a[1] == b[1] && a[2] == b[2]) return a[3] >= b[3];
+	else if (a[1] == b[1]) return a[2] >= b[2];
+	return a[1] >= b[1];
 }
 int main() {
 	ios::sync_with_stdio(false);
@@ -29,7 +29,7 @@ int main() {
 	sort(vec.begin(), vec.end(), compare);
 
 	for (int i = 0; i < n; i++) {
-		if (vec[i][0] == k) cout << i << "\n";
+		if (vec[i][0] == k) cout << i + 1 << "\n";
 	}
 
 
