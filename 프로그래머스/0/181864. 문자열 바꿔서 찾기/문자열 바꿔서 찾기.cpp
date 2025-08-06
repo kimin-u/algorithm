b258f4335943cf728dff51e11dfec37ffe88a50d
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-08-06 21:43:05 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -11,6 +11,12 @@ int solution(string myString, string pat) {
         else tmp+='A';
     }
     
-    if (tmp.find(pat) != string::npos) answer =1;
+    // if (tmp.find(pat) != string::npos) answer =1;
+    for (int i=0; i<myString.length()- pat.length()+1; i++){
+        if (tmp.substr(i, pat.length()) == pat) {
+            answer=1;
+            break;
+        }
+    }
     return answer;
 }
