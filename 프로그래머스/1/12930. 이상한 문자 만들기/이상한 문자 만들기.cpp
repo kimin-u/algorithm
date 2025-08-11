4654d489327f98627cdd10fb1be3e17b0571df60
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-08-11 17:01:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -8,18 +8,17 @@ string solution(string s) {
     string answer = "";
     int idx = -1;
     for (auto c : s){
-        if (c==' ' || c=='\n') idx = -1;
-        if (c>='a' || c>='A') idx++;
-        if (idx == -1){
-            answer += c;
-        }
-        else if (idx %2 ==0){
-            answer += toupper(c);
+        if (c==' ' || c=='\n') {
+            idx = -1;
+            answer+=c;
         }
         else{
-            answer += tolower(c);
-        }
-        
+            idx++;
+            if (idx %2 ==0)
+                answer += toupper(c);
+            else
+                answer += tolower(c);
+        }           
     }
     return answer;
 }
