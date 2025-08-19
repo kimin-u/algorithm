df13aaeaa87213d467cb98b50f38d435ae1bcb0b
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-08-19 17:15:21 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -7,7 +7,6 @@ using namespace std;
 bool compare(int a, int b){
     return a>b;
 }
-
 int solution(vector<int> citations) {
     int answer = 0;
     sort(citations.begin(), citations.end(), compare);
