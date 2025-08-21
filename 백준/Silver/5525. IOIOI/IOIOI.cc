92b8129b254ab4f564662f551759de09098dfa64
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-08-21 14:15:33 +0900

    [Silver I] Title: IOIOI, Time: 12 ms, Memory: 6056 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -17,10 +17,29 @@ int main() {
 		else tmp += 'O';
 	}
 	int answer = 0;
-	
+	/*
 	for (int i = 0; i <= m - (2 * n + 1); i++) {
-		string tt = str.substr(i, 2 * n + 1);
-		if (tt == tmp) answer++;
+		//string tt = str.substr(i, 2 * n + 1);
+		//if (tt == tmp) answer++;
+		bool match = true;
+		for (int j = 0; j < 2 * n + 1; j++) {
+			if (tmp[j] != str[i + j]) {
+				match = false;
+				break;
+			}
+		}
+		if (match) answer++;
+	}
+	*/
+	int i = 0;
+	while (i < m - 1) {
+		int cnt = 0;
+		while (i + 2 < m && str[i] == 'I' && str[i + 1] == 'O' && str[i + 2] == 'I') {
+			cnt++;
+			i += 2; // 한 쌍(OI)씩 이동
+		}
+		if (cnt >= n) answer += (cnt - n + 1); // 겹치는 PN도 포함
+		i++;
 	}
 
 	cout << answer;
