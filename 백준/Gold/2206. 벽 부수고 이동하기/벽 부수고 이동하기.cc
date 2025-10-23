c4aaf7a91108f00a8717b0a6bc3fca98c4f4eb2c
kimin-u <70623862+kimin-u@users.noreply.github.com>
2025-10-23 23:51:37 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 292 ms, Memory: 62496 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -2,41 +2,43 @@
 #include <vector>
 #include <queue>
 #include <string>
+
 using namespace std;
 
 int n,m;
 vector<vector<int>> graph;
 vector<vector<vector<int>>> visited;
 queue<pair<pair<int, int>, int>> q;
+
 vector<int> di = {-1,1,0,0};
 vector<int> dj = {0,0,-1,1};
 
-
-void bfs(int i, int j){
-    q.push({{i,j},0});
-    visited[i][j][0] = 1;
-    
+void bfs(){
+    q.push({{0,0}, 0});
+    visited[0][0][0] = 1;
     while (!q.empty()){
         int ci = q.front().first.first;
         int cj = q.front().first.second;
         int wall = q.front().second;
         q.pop();
         
-        for(int k=0; k<4; k++){
+        for (int k=0; k<4; k++){
             int ni = ci + di[k];
             int nj = cj + dj[k];
 
-            if (ni <0 || ni >=n || nj <0 || nj>=m) continue;
-            if (graph[ni][nj] == 0 && visited[ni][nj][wall] ==0){
+            if (ni <0 || ni >= n || nj<0 || nj>=m) continue;
+
+            if (graph[ni][nj] == 0 && visited[ni][nj][wall] == 0){
                 visited[ni][nj][wall] = visited[ci][cj][wall] + 1;
                 q.push({{ni,nj}, wall});
             }
-            if (graph[ni][nj] == 1 &&  wall ==0){
+            else if (graph[ni][nj] == 1 && wall == 0){
                 visited[ni][nj][1] = visited[ci][cj][wall] + 1;
                 q.push({{ni,nj}, 1});
             }
         }
     }
+
 }
 
 int main(){
@@ -46,13 +48,15 @@ int main(){
     cin>>n>>m;
     graph.assign(n, vector<int>(m));
     visited.assign(n, vector<vector<int>>(m, vector<int>(2,0)));
-    for (int i=0; i<n; i++){
+
+    for(int i=0; i<n; i++){
         string str; cin>>str;
         for (int j=0; j<m; j++){
-            graph[i][j] = str[j] - '0';
+            graph[i][j] = str[j] -'0';
         }
     }
-    bfs(0,0);
+
+    bfs();
 
     int ans0 = visited[n-1][m-1][0];
     int ans1 = visited[n-1][m-1][1];
