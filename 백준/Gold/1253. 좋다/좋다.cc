3b67b33cebd406725c62437f6aa134b0027284e6
김인우 <70623862+kimin-u@users.noreply.github.com>
2026-03-25 23:12:43 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -6,50 +6,53 @@ typedef long long ll;
 using namespace std;
 
 int n;
-vector<int> vec;
+vector<ll> vec;
 
-bool compare(int a, int b){
+bool compare(ll a, ll b){
     return a<b;
 }
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
+
     cin>>n;
     vec.assign(n,0);
-    for (int i=0; i<n; i++)
+    for (int i=0; i<n; i++){
         cin>>vec[i];
-
-    //어떤 수가, 다른 두 수의 합으로 있느냐?
-    // 1= 
-    sort(vec.begin(), vec.end(), compare);
+    }
 
     int answer = 0;
-    
-    for (int k=0; k<n; k++){
-        int left = 0; 
+
+    sort(vec.begin(), vec.end(), compare);
+
+    for (int k= 0; k<n; k++){
+        int left = 0;
         int right = n-1;
-        ll target = vec[k];
+        ll val = vec[k];
 
         while (left < right){
-            ll sum = vec[left] + vec[right];
-
-            if (sum < target){
-                left++;
-            }
-            else if (sum > target){
-                right--;
-            }
-            else{
-                if (left == k) left++;
-                else if (right ==k) right--;
+            if (val == vec[left] + vec[right]){
+                if (left == k){
+                    left++;
+                }
+                else if (right == k) right--;
                 else{
-                    answer++;
+                    answer ++;
                     break;
                 }
+            }
+
+            else if (val < vec[left] + vec[right]){
+                right--;
+            }
+            else {
+                left++;
             }
         }
     }
 
     cout<<answer;
+
+
 }
