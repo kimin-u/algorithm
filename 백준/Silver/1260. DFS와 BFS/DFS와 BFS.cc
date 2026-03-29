6c3af660c0338fc9c9b78c2c640584f349c0218e
김인우 <70623862+kimin-u@users.noreply.github.com>
2026-03-29 21:26:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,67 +1,79 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-
+int n, m, v;
 vector<vector<int>> graph;
 vector<int> visited_dfs;
 vector<int> visited_bfs;
 vector<int> result_dfs;
 vector<int> result_bfs;
 
-void dfs(int i) {
-	visited_dfs[i] = 1;
-	result_dfs.push_back(i);
-	for (int j = 1; j < graph[i].size(); j++) {
-		if (graph[i][j] != 1 || visited_dfs[j] == 1) continue;
-		dfs(j);
-	}
+void dfs(int start){
+    visited_dfs[start] = 1;
+    result_dfs.push_back(start);
+    for (int i = 0; i < graph[start].size(); i++){
+        int nxt = graph[start][i];
+        if (visited_dfs[nxt] == 0) {
+            dfs(nxt);
+        }
+    }
 }
 
-void bfs(int i) {
-	queue<int> q;
-	visited_bfs[i] = 1;
-	result_bfs.push_back(i);
-	q.push(i);
-	while (!q.empty()) {
-		int cur = q.front(); q.pop();
-		for (int j = 1; j < graph[cur].size(); j++) {
-			if (graph[cur][j] != 1 || visited_bfs[j] == 1) continue;
-			q.push(j);
-			visited_bfs[j] = 1;
-			result_bfs.push_back(j);
-		}
-	}
+void bfs(int start){
+    queue<int> q;
+    visited_bfs[start] = 1;
+    q.push(start);
+    result_bfs.push_back(start);
+    while (!q.empty()){
+        int cur = q.front(); q.pop();
+
+        for (int i=0; i<graph[cur].size(); i++){
+            int nxt = graph[cur][i];
+            if (visited_bfs[nxt] == 0){
+                visited_bfs[nxt] =1;
+                q.push(nxt);
+                result_bfs.push_back(nxt);
+            }
+        }
+    }
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-	int n, m, v; cin >> n >> m >> v;
+    cin >> n >> m >> v;
 
-	graph.assign(n + 1, vector<int>(n + 1, -1));
-	visited_dfs.assign(n + 1, 0);
-	visited_bfs.assign(n + 1, 0);
-	for (int i = 0; i < m; i++) {
-		int a, b; cin >> a >> b;
-		graph[a][b] = 1;
-		graph[b][a] = 1;
-	}
-	dfs(v); bfs(v);
-	for (int i = 0; i < result_dfs.size(); i++) {
-		if (i == result_dfs.size() - 1)
-			cout << result_dfs[i] << "\n";
-		else
-			cout << result_dfs[i] << " ";
-	}
-	for (int i = 0; i < result_bfs.size(); i++) {
-		if (i == result_bfs.size() - 1)
-			cout << result_bfs[i] << "\n";
-		else
-			cout << result_bfs[i] << " ";
-	}
+    graph.assign(n, vector<int>());
+
+    for (int i=0; i<m; i++){
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    for (int i = 0; i < n; i++) {
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    visited_dfs.assign(n,0);
+    visited_bfs.assign(n,0);
+
+   dfs(v - 1);
+   bfs(v - 1);
+
+   for (auto v: result_dfs){
+    cout<<v+1<<" ";
+   }
+   cout<<'\n';
+   for (auto v: result_bfs){
+    cout<< v+1<< " ";
+   }
 
 }
