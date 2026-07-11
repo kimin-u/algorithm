c938c8b91d2581ad3eaeffcbfbbf8b6817f8e48d
김인우 <70623862+kimin-u@users.noreply.github.com>
2026-07-11 20:09:38 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -2,36 +2,34 @@
 #include <vector>
 
 using namespace std;
-vector<vector<int>> graph;
+
 vector<int> visited;
 
-void dfs(int i){
+void dfs(int i, vector<vector<int>> &computers){
     visited[i] = 1;
     
-    for (int k=0; k<graph[i].size(); k++){
-        int ni = graph[i][k];
-        if (visited[ni] == 0) {
-            dfs(ni);
+    for (int next = 0; next<computers[i].size(); next++){
+        if (next == i) continue;
+        if (computers[i][next] == 0) continue;
+        if (visited[next] == 0){
+            dfs(next, computers);
         }
     }
+    
+    
 }
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    graph.assign(n,vector<int>());
-    visited.assign(n,0);
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            if (i==j) continue;
-            if (computers[i][j]==1)
-                graph[i].push_back(j);
-        }
-    }
+    
+    visited.assign(n, 0);
     
     for (int i=0; i<n; i++){
         if (visited[i] == 0){
-            dfs(i);
+            dfs(i, computers);
             answer++;
         }
     }
+    
     return answer;
 }
