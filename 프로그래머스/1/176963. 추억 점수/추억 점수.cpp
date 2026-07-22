79fe98debb5a9f4c83b1447c35789832e8d17f4c
김인우 <70623862+kimin-u@users.noreply.github.com>
2026-07-22 22:41:38 +0900

    [level 1] Title: 추억 점수, Time: 0.38 ms, Memory: 5.02 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -2,21 +2,23 @@
 #include <vector>
 #include <unordered_map>
 
+
 using namespace std;
 
 vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
     vector<int> answer;
+    
     unordered_map<string, int> umap;
     for (int i=0; i<name.size(); i++){
         umap[name[i]] = yearning[i];
     }
     
-    for (int i=0; i<photo.size(); i++){
-        int score = 0;
-        for (int j=0; j<photo[i].size(); j++){
-            score += umap[photo[i][j]];
+    for (auto &p : photo){
+        int tmp = 0;
+        for (auto &name : p){
+            tmp += umap[name];
         }
-        answer.push_back(score);
+        answer.push_back(tmp);
     }
     return answer;
 }
